ad44df6f18d2c5a4f63940be959fa87d2eb29355
Paschal Okafor <okaforpaschal018@gmail.com>
2023-02-11 21:41:43 +0100

    Finished Task 7

--- console.py
+++ console.py
@@ -1,22 +1,173 @@
 " The entry point of the command interpreter "
 
 import cmd
+import re
+from models import storage
+from models.base_model import BaseModel
+
 
 class HBNBCommand(cmd.Cmd):
     " The Command Interface class which inherits the Cmd class "
 
     prompt = "(hbnb) "
+    __class_set = set()
+    __all_records = {}
 
     def emptyline(self):
         pass
 
+    def preloop(self):
+        HBNBCommand.__all_records = storage.all()
+        __globals = {**globals()}
+
+        for key in __globals:
+            value = __globals[key]
+
+            if type(value).__name__ == "type" and issubclass(value, BaseModel):
+                HBNBCommand.__class_set.add(key)
+
     def do_quit(self, line):
         " Exits the Console"
         return True
-    
+
     def do_EOF(self, line):
         " Exits the Console"
         return True
+
+    @staticmethod
+    def get_args_from_line(line):
+        if not line:
+            return None
+
+        return line.split(" ")
+
+    @classmethod
+    def __validate_n_run(cls, command, line):
+        '''
+        This class static methods validates input from the console
+        and execute command if validation is successful
+
+        Parameters
+        ----------
+        cls : object - class object
+        command : function - callback function to be executed
+        line : line from the console
+        '''
+        command_name = command.__name__
+
+        line_args = cls.get_args_from_line(line)
+        if not line_args:
+            print("** class name missing **")
+
+        elif line_args[0] not in cls.__class_set:
+            print("** class doesn't exist **")
+
+        else:
+            if command_name == "exec_create":
+                command(line_args[0])
+            elif len(line_args) <= 2:
+                if len(line_args) == 1:
+                    print("** instance id missing **")
+                elif len(line_args) == 2:
+                    [cls_name, id] = line_args
+                    cls_name_id_key = cls_name+"."+id
+
+                    if cls_name_id_key not in HBNBCommand.__all_records:
+                        print("** no instance found **")
+                    else:
+                        command(cls_name_id_key) #execute show & destroy
+            else:
+                if command_name == "exec_update":
+                    command(line_args)
+
+    def do_create(self, line):
+        '''
+        This command creates a new instance of a Model
+
+        Parameter
+        ---------
+        line : str - Name of Class 
+        '''
+        def exec_create(cls_name):
+            " Executes the create command "
+            newObj = globals()[cls_name]()
+            newObj.save()
+            print(newObj.id)
+
+        HBNBCommand.__validate_n_run(exec_create, line)
+    
+    def do_show(self, line):
+        '''
+        This command shows details of an existing instance
+
+        Parameters
+        ---------
+        line : str - Name of Class and id
+
+        '''
+        def exec_show(cls_name_id_key):
+            " Executes the show command "
+            cls_name = cls_name_id_key.split(".")[0]
+            selected_rec_obj = HBNBCommand.__all_records[cls_name_id_key]
+            print(selected_rec_obj.__str__())
+
+        HBNBCommand.__validate_n_run(exec_show, line)
+
+    def do_destroy(self, line):
+        '''
+        This command destroys an existing instance
+
+        Parameters
+        ---------
+        line : str - Name of Class and id
+
+        '''
+        def exec_destroy(cls_name_id_key):
+            " Executes the destroy command "
+            HBNBCommand.__all_records.pop(cls_name_id_key)
+            storage.save()
+
+        HBNBCommand.__validate_n_run(exec_destroy, line)
+
+    def do_all(self, line):
+        data_list = []
+        all_records = HBNBCommand.__all_records
+
+        if not line:
+            for key in all_records:
+                data_list.append(all_records[key].__str__())
+            print(data_list)
+        elif line not in HBNBCommand.__class_set:
+            print("** class doesn't exist **")
+        else:
+            for key in all_records:
+                if all_records[key].__class__.__name__ == line:
+                    data_list.append(all_records[key].__str__())
+            print(data_list)
+    
+    def do_update(self, line):
+        " update <class name> <id> <attribute name> '<attribute value>'"
+
+        def exec_update(line_args):
+            " Executes the update command "
+
+            if len(line_args) == 2:
+                print("** attribute name missing **")
+            elif len(line_args) == 3:
+                print("** value missing **")
+            else:
+                [cls_name, id, attr_name, attr_value] = line_args
+                all_records = HBNBCommand.__all_records
+                cls_name_id_key = cls_name+"."+id
+                selected_record = all_records[cls_name_id_key]
+
+                if attr_name not in ["id", "created_at", "updated_at"]:
+                    selected_record.__setattr__(attr_name, eval(attr_value))
+                    selected_record.save()
+
+        HBNBCommand.__validate_n_run(exec_update, line)
+
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
