f1b5c0a108849024a9f88e78b2aaf5d2e8be0543
Paschal Okafor <okaforpaschal018@gmail.com>
2023-02-11 17:20:49 +0100

    Finished Task 5 and amended BaseModel Class

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,7 @@ for other models created
 
 from uuid import uuid4
 from datetime import datetime
+import models as m
 
 class BaseModel:
     '''
@@ -48,14 +49,19 @@ class BaseModel:
             self.id = uuid4().__str__()
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            m.storage.new(self)
 
     def __str__(self):
         " Returns the string representation of an instance of the Base Model class "
         return("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
     
     def save(self):
-        " Updates the public instance attribute updated_at with the current datetime "
+        '''
+        Updates the public instance attribute updated_at with the current datetime 
+        and persists instance to file storage
+        '''
         self.updated_at = datetime.now()
+        m.storage.save()
 
     def to_dict(self):
         " returns a dictionary containing all keys/values of __dict__ of the instance "
